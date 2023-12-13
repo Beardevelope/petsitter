46cccba84a1ee574a19fe7829f02f88ba337f5da
Luizz Lee <leehanvit511@gmail.com>
2023-12-13 19:48:23 +0900

    feat[reservattion] dev 머지 sitter 기능 추가 유저 생성 실패

--- src/router/reservationRouter.js
+++ src/router/reservationRouter.js
@@ -1,12 +1,13 @@
 import ReservationController from "../controller/reservationController.js";
 import { Router } from 'express';
+import { needSignin } from "../middleware/signin_middleware.js";
 
 const reservationRouter = Router();
 const reservationController = new ReservationController();
 
-reservationRouter.post('/', reservationController.createController);
-reservationRouter.get('/', reservationController.getAll);
-reservationRouter.put('/:reservationId', reservationController.updateController);
-reservationRouter.delete('/:reservatonId', reservationController.deleteController);
+reservationRouter.post('/', reservationController.createController, needSignin);
+reservationRouter.get('/', reservationController.getAll, needSignin);
+reservationRouter.put('/:reservationId', reservationController.updateController, needSignin);
+reservationRouter.delete('/:reservatonId', reservationController.deleteController, needSignin);
 
 export { reservationRouter };
