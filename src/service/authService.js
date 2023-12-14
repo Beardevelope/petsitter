549d6c115974caee948d3dbda175628b8813ca26
minimini1212 <zjonyv12@naver.com>
2023-12-14 20:22:24 +0900

    feat: [feat-minhee-auth] 회원가입, 로그인 생성
    - authRouter, Controller, Service, Repository 생성
    - 회원가입, 로그인 기능 구현 확인

--- src/service/authService.js
+++ src/service/authService.js
@@ -1,18 +1,75 @@
-import { AuthRepository } from "../repository/authSitterRepository.js";
+import { AuthRepository } from "../repository/authRepository.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
 export class AuthService {
   authRepository = new AuthRepository();
 
-  // 회원가입
-  signUpUser = async (
-    sitterEmail,
-    sitterName,
-    nickName,
+  // 일반유저 회원가입
+  signUpNormalUser = async (email, role, password, rePassword) => {
+    // 회원가입 비밀번호 조건
+    function checkPwd(str_pwd) {
+      const reg = /^[A-Za-z\d@$!%*#?&]{6,}$/;
+      return reg.test(str_pwd); //정규식과 매치되면 true, 매치되지않으면 false 반환.
+    }
+
+    //회원가입 이메일 조건
+    function checkEmail(str_email) {
+      const reg = /^[A-z0-9]+@[a-z]+\.[a-z]{2,3}$/;
+      return reg.test(str_email);
+    }
+
+    // checkEmail 함수를 이용한 이메일 조건을 만족하는가
+    if (!checkEmail(email)) {
+      const errors = new Error("이메일 형식을 맞춰주세요");
+      errors.statusCode = 400;
+      throw errors;
+    }
+
+    // checkPwd 함수를 이용한 패스워드 조건을 만족하는가
+    if (!checkPwd(password)) {
+      const errors = new Error("비밀번호는 최소 6자리 이상이어야 합니다.");
+      errors.statusCode = 400;
+      throw errors;
+    }
+
+    // 패스워드, 패스워드 검증 값이 일치하는가
+    if (password !== rePassword) {
+      const errors = new Error("비밀번호가 일치하지 않습니다.");
+      errors.statusCode = 409;
+      throw errors;
+    }
+
+    // email을 포함하는 객체찾기
+    const existUser = await this.authRepository.findUserByEmail(email);
+    if (existUser) {
+      const errors = new Error("email이 이미 사용 중입니다.");
+      errors.statusCode = 409;
+      throw errors;
+    }
+    // 비밀번호 암호화
+    const hashPassword = bcrypt.hashSync(
+      password,
+      parseInt(process.env.PASSWORD_HASH_SALT_ROUNDS)
+    );
+
+    const user = await this.authRepository.createUser(
+      email,
+      role,
+      hashPassword
+    );
+
+    return user;
+  };
+
+  // 시터 유저 회원가입
+  signUpSitterUser = async (
+    email,
+    role,
     career,
+    name,
     password,
-    Repassword
+    rePassword
   ) => {
     // 회원가입 비밀번호 조건
     function checkPwd(str_pwd) {
@@ -27,7 +84,7 @@ export class AuthService {
     }
 
     // checkEmail 함수를 이용한 이메일 조건을 만족하는가
-    if (!checkEmail(sitterEmail)) {
+    if (!checkEmail(email)) {
       const errors = new Error("이메일 형식을 맞춰주세요");
       errors.statusCode = 400;
       throw errors;
@@ -48,14 +105,14 @@ export class AuthService {
     }
 
     // 패스워드, 패스워드 검증 값이 일치하는가
-    if (password !== Repassword) {
+    if (password !== rePassword) {
       const errors = new Error("비밀번호가 일치하지 않습니다.");
       errors.statusCode = 409;
       throw errors;
     }
 
     // email을 포함하는 객체찾기
-    const existUser = await this.authRepository.findUserByEmail(sitterEmail);
+    const existUser = await this.authRepository.findUserByEmail(email);
     if (existUser) {
       const errors = new Error("email이 이미 사용 중입니다.");
       errors.statusCode = 409;
@@ -67,21 +124,31 @@ export class AuthService {
       parseInt(process.env.PASSWORD_HASH_SALT_ROUNDS)
     );
 
-    const user = await this.authRepository.createUser(
-      sitterEmail,
-      sitterName,
-      nickName,
-      career,
+
+    const sitterUser = await this.authRepository.createUser(
+      email,
+      role,
       hashPassword
     );
+    // 이메일로 가져온 userId 가져오기
+    const userId = sitterUser.userId
+
+    const detailUser = await this.authRepository.createSitterDetails(name, career, userId);
+
+
+    const user = {
+      ...sitterUser,
+      ...detailUser
+    }
+
 
     return user;
   };
 
   // 로그인
-  signInUser = async (sitterEmail, password) => {
+  signInUser = async (email, password) => {
     // email로 유저가 존재하는지 확인
-    const existUser = await this.authRepository.findUserByEmail(sitterEmail);
+    const existUser = await this.authRepository.findUserByEmail(email);
     if (!existUser) {
       const errors = new Error("사용자가 존재하지 않습니다.");
       errors.statusCode = 400;
@@ -96,7 +163,7 @@ export class AuthService {
     }
     // 토큰의 만료시간 설정 12시간..
     const token = jwt.sign(
-      { sitterId: existUser.sitterId },
+      { userId: existUser.userId },
       process.env.JWT_ACCESS_TOKEN_SECRET,
       {
         expiresIn: "12h",
