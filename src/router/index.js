c9d034badb456d1e4cc0a7bef19bfe200f79c58d
sunah <1004_sunah@naver.com>
2023-12-15 03:24:02 +0900

    feat [ userSitterRouter ] index에서 주석

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,12 @@
 import { Router } from 'express';
 import { reviewRouter } from './reviewRouter.js';
 import { reservationRouter } from './reservationRouter.js';
-import { userSitterRouter } from './userSitterRouter.js';
+// import { userSitterRouter } from './userSitterRouter.js';
 import { petRouter } from './petsRouter.js';
 
 const apiRouter = Router();
 
-apiRouter.use('/user', userSitterRouter);
+// apiRouter.use('/user', userSitterRouter);
 apiRouter.use('/pet', petRouter);
 apiRouter.use('/reservations', reservationRouter);
 apiRouter.use('/review', reviewRouter);
